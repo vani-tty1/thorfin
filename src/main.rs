8cc29e57e866b9d0e4f89e1fa7d9a0076898631b
Giovanni <giovannirafanan609@gmail.com>
2026-01-15 20:19:07 +0800

    test spinner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod window;
+use window::window_init;
+use libadwaita as adw;
+use adw::prelude::*;
+use adw::{Application};
+
+
+
+
 fn main() {
-    println!("Hello World");
+    let app = Application::builder()
+        .application_id("io.github.vani1_2.thorfin")
+        .build();
+    app.connect_activate(window_init);
+    app.run();
 }
---
+++ src/window.rs
@@ -0,0 +1,43 @@
+use libadwaita as adw;
+use adw::prelude::*;
+use adw::{HeaderBar, ApplicationWindow, Application};
+use gtk4::{self as gtk};
+use gtk::{Box};
+
+pub fn window_init(main: &Application) {
+    //notes to self:
+    // this thing is like this:
+    // dont forgor it future self
+    // Window
+    //  └── content(The (Parent) Vertical Box)
+    //      ├── 1. head_bar (Added first -> Sits at the Top)
+    //      └── 2. main_dis (Added second -> Sits at the Bottom)
+    //              └── spinner (Inside main_dis)
+    let content = Box::new(gtk4::Orientation::Vertical, 0);
+    let head_bar = HeaderBar::builder()
+        .build();
+    content.append(&head_bar);
+
+    
+    let main_dis = Box::builder()
+        .valign(gtk4::Align::Center)
+        .halign(gtk4::Align::Center)
+        .hexpand(true)
+        .vexpand(true)
+        .build();
+    
+    let spinner = adw::Spinner::new();
+    spinner.set_size_request(128, 128);
+    main_dis.append(&spinner);
+    
+    content.append(&main_dis);
+    
+    let window = ApplicationWindow::builder()
+        .application(main)
+        .title("Thorfin")
+        .default_width(500)
+        .default_height(300)
+        .content(&content) 
+        .build();
+    window.present();
+}
