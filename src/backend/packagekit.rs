2a6af41cdc72fcecb5b91a6db23d3a7ddd750683
vani-tty1 <giovannirafanan609@gmail.com>
2026-02-21 18:01:08 +0800

    test fetch in distro repo

--- src/backend/packagekit.rs
+++ src/backend/packagekit.rs
@@ -2,13 +2,9 @@ use std::process::Command;
 use crate::app_display::AppEntry;
 use std::path::Path;
 
-fn is_flatpak() -> bool {
-    Path::new("/.flatpak-info").exists()
-}
-
 pub async fn update_list() {
     let (cmd, args) = if is_flatpak() {
-        ("flatpak-spawn", vec!["--host", "pkcon", "refresh"])
+        ("flatpak-spawn", vec!["--host", "pkgcli", "refresh", "force"])
     } else {
         ("pkcon", vec!["refresh"])
     };
@@ -21,7 +17,7 @@ pub async fn update_list() {
 pub async fn install_app(app: &AppEntry) {
     let app_name = app.name.to_lowercase();
     let (cmd, args) = if is_flatpak() {
-        ("flatpak-spawn", vec!["--host", "pkcon", "install", "-y", &app_name])
+        ("flatpak-spawn", vec!["--host", "pkgcli", "install", "-y", &app_name])
     } else {
         ("pkcon", vec!["install", "-y", &app_name])
     };
@@ -29,4 +25,55 @@ pub async fn install_app(app: &AppEntry) {
     if let Err(e) = Command::new(cmd).args(args).status() {
         eprintln!("Failed to run install for {}: {}", app.name, e);
     }
+}
+
+
+fn is_flatpak() -> bool {
+    Path::new("/.flatpak-info").exists()
+}
+
+pub async fn search_repo(query: &str) -> Vec<AppEntry> {
+    let (cmd, args) = if is_flatpak() {
+        ("flatpak-spawn", vec!["--host", "pkgcli", "--plain", "search", "name", query])
+    } else {
+        ("pkcon", vec!["--plain", "search", "name", query])
+    };
+
+    println!("Running: {} {:?}", cmd, args);
+
+    let Ok(output) = Command::new(cmd).args(args).output() else {
+        eprintln!("Failed to execute command!");
+        return vec![];
+    };
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    println!("--- PKCON RAW OUTPUT ---");
+    println!("{}", stdout);
+    println!("------------------------");
+
+    let mut apps = Vec::new();
+    
+    for line in stdout.lines() {
+        if line.starts_with("Available") || line.starts_with("Installed") {
+            let parts: Vec<&str> = line.split_whitespace().collect();
+            
+            if parts.len() >= 3 {
+                let full_package_name = parts[1];
+                let name = full_package_name.split('-').next().unwrap_or("Unknown").to_string();
+                
+                let summary = if parts.len() >= 4 {
+                    parts[3..].join(" ")
+                } else {
+                    format!("Package: {}", full_package_name) // Fallback summary
+                };
+    
+                apps.push(AppEntry {
+                    id: full_package_name.to_string(), 
+                    name,
+                    summary,
+                });
+            }
+        }
+    }
+    apps
 }
